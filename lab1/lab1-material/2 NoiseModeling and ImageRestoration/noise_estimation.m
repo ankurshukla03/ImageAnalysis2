cfff32665f18b8d3421d23395efa60546c352230
Tristan <thrawst88@gmail.com>
2018-01-28 14:58:34 +0100

    template matching 17-20

--- lab1/lab1-material/2 NoiseModeling and ImageRestoration/noise_estimation.m
+++ lab1/lab1-material/2 NoiseModeling and ImageRestoration/noise_estimation.m
@@ -2,12 +2,8 @@ cm = imread('cameraman noise.tif');
 
 roi = roipoly(cm,[180 230],[20 130]);
 
-%selecting region of interest by mouse 
-
 %
-histogram = histroi(roi,0,256);
+histogram = histroi(roi,[180 230],[20 130]);
 
 %n=10 
 stats = statmoments(roi,10);
-
-
---
+++ lab1/lab1-material/3 TemplateMatching/phase_matching.m
@@ -0,0 +1,22 @@
+clear all; 
+close all;
+
+img = rgb2gray(imread('search.png'));
+template = rgb2gray(imread('template1.png'));
+% template = rgb2gray(imread('template2.png'));
+
+pad_x = size(img, 2); %cols
+pad_y = size(img, 1); %rows
+
+F = fft2(img);
+G = fft2(template, pad_y, pad_x);
+
+% cross power
+nom = F .* conj(G);
+denom = abs(F .* conj(G));
+cp = real(ifft2(nom ./ denom));
+
+[y, x] = find(cp == max(cp(:)));
+
+imshow(img);
+rectangle('Position',[x, y, size(template,1), size(template,2)]);
