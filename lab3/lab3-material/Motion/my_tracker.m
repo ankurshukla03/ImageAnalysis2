538fe8f2f9dff8ff950f10edd73d5dca3bd6fc4a
Tristan <thrawst88@gmail.com>
2018-02-28 20:21:50 +0100

    clean up and comments

--- lab3/lab3-material/Motion/my_tracker.m
+++ lab3/lab3-material/Motion/my_tracker.m
@@ -1,8 +1,7 @@
 close all;
 
 %% open video files
-% read 250 of 581
-start_f = 100;
+start_f = 250;
 end_f = 400;
 assert(start_f < end_f);
 mov = read_movie('source_sequence.avi', start_f, end_f);
@@ -20,9 +19,11 @@ colors = ["red", "green", "blue", "yellow", "magenta", "cyan"];
 
 %% processing
 progressbar('frame');
-prev_points = inf(5,2);
-next_points = prev_points;
-prev_colors = strings(5,1);
+
+% historical variables
+prev_points = inf(5,2);  % last positions
+cur_points = prev_points; % current points
+prev_colors = strings(5,1); % colors for each of the points
 for i=1:(end_f-start_f)
     % get the current frame, mask out the center
     frame = mov(:,:,i);
@@ -31,7 +32,7 @@ for i=1:(end_f-start_f)
     h = fspecial('average', 3);
     c_x = imfilter(frame, h, 'replicate');
     % binarize on some intensity
-    c_x = c_x < 100; 
+    c_x = c_x < 100;
 
     Lb_Img = bwlabel(c_x);
     Comps = bwconncomp(Lb_Img);
@@ -42,7 +43,7 @@ for i=1:(end_f-start_f)
     imh = imshow(frame);
     hold on;
     for el=1:numel(props)
-        if props(el).Area > 90
+        if props(el).Area > 90 % skip background object.
             continue;
         end
         % init prev points
@@ -50,7 +51,7 @@ for i=1:(end_f-start_f)
         y = props(el).Centroid(2);
         cur = [x,y];
         if all(prev_points(el,:) == Inf)
-            next_points(el,:) = cur;
+            cur_points(el,:) = cur;
             c_color = colors(el);
             prev_colors(el) = c_color;
         else 
@@ -59,13 +60,13 @@ for i=1:(end_f-start_f)
                 (prev_points(:,2)-cur(:,2)).^2);
             [~, ind] = min(dist);
             c_color = prev_colors(ind);
-            next_points(ind,:) = cur;
+            cur_points(ind,:) = cur;
         end
         line(x,y,'marker','o','markersize',10,'LineWidth',2,...
             'Color', c_color);
     end
-    prev_points = next_points;
-    next_points = inf(5,2);
+    prev_points = cur_points;
+    cur_points = inf(5,2);
     F = getframe(fig);
     writeVideo(output,F);
     progressbar(i/(end_f - start_f));
